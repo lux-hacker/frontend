95a8f83f671ca24e623790db0dd281ad0a7d72ce
Pustovit Vladislav <pvs013@campus.mephi.ru>
2024-03-02 13:44:05 +0300

    Fix

--- lab5/script2.js
+++ lab5/script2.js
@@ -1,8 +1,8 @@
 function User(name, age){
     this.name = name
     this.age = age
+}
 
-    this.hello = function(){
-        console.log(`Hi! My name if ${this.name}. And I am ${this.age} years old.`)
-    }
+User.prototype.hello = function(){
+    console.log(`Hi! My name if ${this.name}. And I am ${this.age} years old.`)
 }
